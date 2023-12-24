cf2a7ddef96b50a0525103b838a4c4333bc9e1b6
Susan Wu <tzwukerf@gmail.com>
2023-12-24 07:40:52 -0500

    Update block-game.js

--- Blockers2.io/block-game.js
+++ Blockers2.io/block-game.js
@@ -25,31 +25,37 @@ var points = 0;
 
 var number;
 
+var free = new Array(row * cols);
+
 window.onload = setup();
 
+function keyDown(e) {
+
+
+}
+
 function setup() {
-	//if (validDim() == false) {
+  //if (validDim() == false) {
   //	console.log("error");
   //} else{
-    List = initialSet(rows, cols);
-    rows = List[0];
-    cols = List[1];
-    randomGoal = List[2];
-    randomInitial = List[3];
-    randomInitialValue = List[3];
-    x = 0;
-    number = ((rows*cols)-(cols-1));
+  List = initialSet(rows, cols);
+  rows = List[0];
+  cols = List[1];
+  randomGoal = List[2];
+  randomInitial = List[3];
+  randomInitialValue = List[3];
+  x = randomInitial;
+  number = ((rows * cols) - (cols - 1));
   //}
   return;
 }
 
-function validDim(){
-	rows = parseInt(sessionStorage.getItem('rows'));
+function validDim() {
+  rows = parseInt(sessionStorage.getItem('rows'));
   cols = parseInt(sessionStorage.getItem('cols'));
   console.log(cols)
-	if (rows <= 0 || cols <= 0 || (rows == 1 && (cols == 1 || cols == 2) || (cols == 1 && (rows == 1 || rows == 2)) || isNaN(rows) || isNaN(cols))
-  ) {
-  	document.getElementById("error-para").innerHTML = "Invalid Row or Column number: Fatal Error";
+  if (rows <= 0 || cols <= 0 || (rows == 1 && (cols == 1 || cols == 2) || (cols == 1 && (rows == 1 || rows == 2)) || isNaN(rows) || isNaN(cols))) {
+    document.getElementById("error-para").innerHTML = "Invalid Row or Column number: Fatal Error";
     return false;
   }
   return true;
@@ -57,25 +63,25 @@ function validDim(){
 
 
 function initialSet(r, c) {
-	List = makeRows(r, c);
+  List = makeRows(r, c);
   rows = List[0];
   cols = List[1];
   mult = rows * cols;
-  randomGoal = Math.round(Math.random() * (mult-1)) + 1;
-	randomInitial = Math.round(Math.random() * (mult-1)) + 1;
+  randomGoal = Math.round(Math.random() * (mult - 1)) + 1;
+  randomInitial = Math.round(Math.random() * (mult - 1)) + 1;
   bluePortal = cols;
-  purplePortal = (rows*cols)-(cols-1);
- 	while(randomGoal == cols || randomGoal == (mult-(cols-1))){
-  	randomGoal = Math.round(Math.random() * (mult-1)) + 1;
+  purplePortal = (rows * cols) - (cols - 1);
+  while (randomGoal == cols || randomGoal == (mult - (cols - 1))) {
+    randomGoal = Math.round(Math.random() * (mult - 1)) + 1;
   }
-  while(randomInitial == cols || randomInitial == (mult-(cols-1)) || randomInitial == randomGoal){
-  	randomInitial = Math.round(Math.random() * (mult-1)) + 1;
+  while (randomInitial == cols || randomInitial == (mult - (cols - 1)) || randomInitial == randomGoal) {
+    randomInitial = Math.round(Math.random() * (mult - 1)) + 1;
   }
   document.getElementById("grid" + randomInitial).innerText = "O";
   document.getElementById("grid" + randomGoal).innerText = "Goal";
-  document.getElementById("grid"+ purplePortal).innerText = "P";
-  document.getElementById("grid"+ bluePortal).innerText = "B";
-  
+  document.getElementById("grid" + purplePortal).innerText = "P";
+  document.getElementById("grid" + bluePortal).innerText = "B";
+
   return [rows, cols, randomGoal, randomInitial];
 }
 
@@ -89,7 +95,7 @@ function makeRows(rows, cols) {
     cell.id = "grid" + i;
 
   }
-  
+
   return [rows, cols];
 }
 
@@ -98,28 +104,28 @@ console.log(checkerBFS(x, 1));
 
 function checkerBFS(x, mode) {
 
-	// 1: character is blue and can only break 1
+  // 1: character is blue and can only break 1
   // 2: character is purple and can only break 2
 
-	var visited = new Array(rows*cols + 1);
-  for(var i = 0; i < visited.length; i++) {
-		visited[i] = false;
+  var visited = new Array(rows * cols + 1);
+  for (var i = 0; i < visited.length; i++) {
+    visited[i] = false;
   }
   var queue = [];
   queue.push(x);
-  
+
   visited[x] = true;
-  
+
   while (queue.length > 0) {
-  	var head = queue[0];
+    var head = queue[0];
     queue.shift();
     if (head == randomGoal || head == purplePortal || head == bluePortal) {
-    	return true;
+      return true;
     }
     for (var i = 0; i < 8; i++) {
-    	var surrounding = [head - cols - 1, head - cols, head - cols + 1, head + 1, head + cols + 1, head + cols, head + cols - 1, head - 1];
+      var surrounding = [head - cols - 1, head - cols, head - cols + 1, head + 1, head + cols + 1, head + cols, head + cols - 1, head - 1];
       if (surrounding[i] <= (rows * cols) && surrounding[i] > 0) {
-      	var temp = document.getElementById("grid" + surrounding[i]).innerHTML;
+        var temp = document.getElementById("grid" + surrounding[i]).innerHTML;
         if (!visited[surrounding[i]] && (temp == mode.toString() || temp == " ")) {
           queue.push(surrounding[i]);
         }
@@ -128,3 +134,61 @@ function checkerBFS(x, mode) {
   }
   return false;
 }
+
+//these are the moving functions
+//even though most of the code is similar, it would be easier
+//for me in the future to make these all different functions
+//example: left(1) instead of move(left, 1)
+
+function left(mode) {
+  try {
+    var next = document.getElementById("grid" + x - 1).innerText;
+    if (next == mode || next == " " || x - 1 == randomGoal || x - 1 == bluePortal || x - 1 == purplePortal) {
+      x--;
+      return true;
+    }
+    return false;
+  } catch (err) {
+    return false;
+  }
+}
+
+function up(mode) {
+  try {
+    var next = document.getElementById("grid" + x - cols).innerText;
+    if (next == mode || next == " " || x - cols == randomGoal || x - cols == bluePortal || x - cols == purplePortal) {
+      x -= cols;
+      return true;
+    }
+    return false;
+  } catch (err) {
+    return false;
+  }
+}
+
+function down(mode) {
+  try {
+    var next = document.getElementById("grid" + x + cols).innerText;
+    if (next == mode || next == " " || x + cols == randomGoal || x + cols == bluePortal || x + cols == purplePortal) {
+      x += cols;
+      return true;
+    }
+    return false;
+  } catch (err) {
+    return false;
+  }
+}
+
+function right(mode) {
+  try {
+    var next = document.getElementById("grid" + x + 1).innerText;
+    if (next == mode || next == " " || x + 1 == randomGoal || x + 1 == bluePortal || x + 1 == purplePortal) {
+      x++;
+      return true;
+    }
+    return false;
+  } catch (err) {
+    return false;
+  }
+}
+
